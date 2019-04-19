64ab783a7798e4a9c5a8501c48471731828e4ed0
Omkar Todkar <omkar@taglr.com>
2019-04-19 19:01:44 +0530

    implement a sample test.

--- app/src/test/java/com/todkars/ExampleUnitTest.kt
+++ app/src/test/java/com/todkars/ExampleUnitTest.kt
@@ -18,22 +18,44 @@ https://github.com/omtodkar/ShimmerRecyclerView/blob/master/LICENSE.md
 */
 package com.todkars
 
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.activity_example.*
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.junit.MockitoJUnitRunner
+import org.robolectric.Robolectric
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.android.controller.ActivityController
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-@RunWith(MockitoJUnitRunner::class)
+@RunWith(RobolectricTestRunner::class)
 class ExampleUnitTest {
+
+    lateinit var activity: ActivityController<ExampleActivity>
 
     @Before
     @Throws(Exception::class)
     fun setUp() {
+        activity = Robolectric.buildActivity(ExampleActivity::class.java)
+    }
+
+    @Test
+    fun test_initial_layout_manager_is_linear_layout_manager() {
+        // given
+        // fast-forward activity to resumed state.
+        activity.create().start().resume()
+
+        // when
+        val recyclerView = activity.get().user_listing
+
+        // then
+        Assert.assertTrue(recyclerView.layoutManager !is GridLayoutManager)
     }
 
     @After
